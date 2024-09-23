633b529c1e6f8e0c76cd0dffae13047808980177
dumao99read <liujiahua@qq.com>
2024-09-23 22:10:39 +0800

    更新tools中的方法。

--- tools/file_setting.py
+++ tools/file_setting.py
@@ -1,12 +1,4 @@
 class ExcelSetting():
-    """重写：读取Excel配置"""
-    @staticmethod
-    def getExcelColToDict(sheet_name, colkey, colvalue, filepath='config/自动化配置.xlsx'):
-        filepath = os.path.join(ROOT_PATH,filepath)
-        file = pd.read_excel(filepath, sheet_name=sheet_name)
-        dict_list = {}
-        for k,v in zip(file[colkey], file[colvalue]):
-            dict_list[k] = v
-        return dict_list
-
+    """之前主要是美化Excel的方法"""
+    pass
 

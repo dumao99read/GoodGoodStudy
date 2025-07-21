9e1ba3e5401adebe468d906f6e65b3ea2a1550f7
dumao99read <liujiahua@qq.com>
2025-07-21 22:55:57 +0800

    完善pytest框架。

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
+import os
+
 import pytest
 from playwright.sync_api import sync_playwright
+from api.aw import PostPlaform
 
 ui_keywords = ['ui_baidu', 'ui_douban']
 api_keywords = ['api_baidu', 'api_douban']
@@ -29,6 +32,17 @@ def page(browser, request):
         yield None
 
 @pytest.fixture(autouse=True)
+def post_platform(request):
+    if any(keyword in request.node.keywords for keyword in ui_keywords):
+        yield None
+    elif any(keyword in request.node.keywords for keyword in api_keywords):
+        post_platform = PostPlaform()  # post接口请求对象
+        yield post_platform
+        print('post_platform的后置预留。')
+    else:
+        yield None
+
+@pytest.fixture(autouse=True)
 def before_each_after_each(page, request):
     if 'ui_baidu' in request.node.keywords:
         page.goto("http://www.baidu.com")
@@ -39,7 +53,13 @@ def before_each_after_each(page, request):
         yield page
 
     elif any(keyword in request.node.keywords for keyword in api_keywords):
-        yield None
+        yield post_platform
+        print('before_each_after_each的后置预留。')
 
     else:
         yield None
+
+
+def pytest_sessionfinish():
+    os.system("allure generate ./temps -o ./report --clean")
+    pass
